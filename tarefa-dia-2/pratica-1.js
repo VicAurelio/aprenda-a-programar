00bd5304d9051a7c2eeda5065dac849cb9d5b97a
Victor Bastos <victoraurelio.1989@gmail.com>
2022-08-24 20:13:35 -0300

    melhorando template string

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -6,4 +6,6 @@ let cidade = "Grécia";
 
 console.log(nome, email, profissao, idade, cidade);
 
-console.log(`Olá,`, nome, `! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para`, cidade, `. Confirma pra gente alguns dados? A sua idade é`, idade, `, você é`, profissao, `e o seu email é`, email, `? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
+//console.log(`Olá,`, nome, `! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para`, cidade, `. Confirma pra gente alguns dados? A sua idade é`, idade, `, você é`, profissao, `e o seu email é`, email, `? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${cidade}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
