8a1715799a6c21f03a1dc89fbebf89704a39e6a4
Victor Bastos <victoraurelio.1989@gmail.com>
2022-08-24 18:53:05 -0300

    adicionando resumo dia 1

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,22 @@
 console.log("Hello World");
-console.log("Olá mundo")
+console.log("Olá mundo");
+
+// duas barras são comentários e não executados pelo código
+// let é uma palavra chave, reservada
+
+let minhaVariavel = "ITuring";
+let outraVariavel = 1;
+
+minhaVariavel = "Teste";
+outraVariavel = 2;
+
+console.log(minhaVariavel);
+console.log(outraVariavel);
+
+let variavel1 = "Texto aqui";
+let variavel2 = variavel1;
+console.log(variavel2);
+
+let numero1 = 100;
+let numero2 = 200;
+console.log(numero1 + numero2);
