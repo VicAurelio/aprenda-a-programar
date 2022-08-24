1af9709d48e6bc4c265dee8b776bdf77d3771aff
Victor Bastos <victoraurelio.1989@gmail.com>
2022-08-24 19:23:03 -0300

    completando tarefas + desafios

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -5,3 +5,5 @@ let idade = 32;
 let cidade = "Grécia";
 
 console.log(nome, email, profissao, idade, cidade);
+
+console.log(`Olá,`, nome, `! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para`, cidade, `. Confirma pra gente alguns dados? A sua idade é`, idade, `, você é`, profissao, `e o seu email é`, email, `? Ficamos no aguardo. Muito obrigado e boa viagem! :D`);
